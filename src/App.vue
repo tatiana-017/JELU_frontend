0e86e083db3409d1d9041081b0114ede645b7f42
Jeimy Arias <tr2303100@gmail.com>
2021-10-29 13:14:34 -0500

    prueba 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,178 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+
+    <div class="header">
+      <h1> Camaras JELU </h1>
+      <nav>
+        <button v-if="is_auth" v-on:click="loadHome"> Inicio </button>
+        <button v-if="is_auth" v-on:click="logOut"> Cerrar Sesión </button>>
+        <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadSignUp" > Registrarse </button>
+        <button v-if="is_auth" v-on:click="loadHome"> Inicio </button>
+      </nav>
+    </div>
+
+    <div class="main-component">
+      <router-view  
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+        v-on:logOut="logOut"
+      >
+      </router-view>
+    </div>
+    
+
+    <div class="footer">
+      <h2>Misión TIC 2022</h2>
+    </div>
+
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  name: 'App',
+
+  data: function(){
+      return{
+        is_auth: false
+      }
+  },
+
+  components: {
+  },
+
+  methods:{
+    verifyAuth: function() {
+      this.is_auth = localStorage.getItem("isAuth") || false;
+
+      if (this.is_auth == false)
+        this.$router.push({ name: "logIn" });
+      else
+        this.$router.push({ name: "home" });
+    },
+
+    loadLogIn: function(){
+      this.$router.push({name: "logIn"})
+    },
+
+    loadSignUp: function(){
+      this.$router.push({name: "signUp"})
+    },
+
+    completedLogIn: function(data) {
+      localStorage.setItem("isAuth", true);
+      localStorage.setItem("username", data.username);
+      localStorage.setItem("token_access", data.token_access);
+      localStorage.setItem("token_refresh", data.token_refresh);
+      alert("Autenticación Exitosa");
+      this.verifyAuth();
+    },
+
+    completedSignUp: function(data) {
+      alert("Registro Exitoso");
+      this.completedLogIn(data);
+    },
+
+    loadHome: function() {
+      this.$router.push({ name: "home" });
+    },
+    logOut: function () {
+      localStorage.clear();
+      alert("Sesión Cerrada");
+      this.verifyAuth();
+    },
+  },
+
+  created: function(){
+    this.verifyAuth()
+  }
+}
+</script>
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  body{
+    margin: 0 0 0 0;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .header{
+    margin: 0%;
+    padding: 0;
+    width: 100%;
+    height: 10vh; 
+    min-height: 100px;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    background-color: #283747 ;
+    color:#E5E7E9  ;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+
+  .header nav {
+    height: 100%;
+    width: 20%;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 20px;
+  }
+
+  .header nav button{
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
+
+  .header nav button:hover{
+    color: #283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
+  }
+
+  
+  .main-component{
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+
+    background: #FDFEFE ;
+  }
+
+ 
+  .footer{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px; 
+
+    background-color: #283747;
+    color: #E5E7E9;
+
+  }
+
+  .footer h2{
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
---
+++ src/router.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+import App from './App.vue';
+
+import LogIn from './components/LogIn.vue'
+import SignUp from './components/SignUp.vue'
+import Home from './components/Home.vue'
+
+const routes = [{
+    path: '/',
+    name: 'root',
+    component: App
+},
+{
+    path: '/user/logIn',
+    name: "logIn",
+    component: LogIn
+},
+{
+    path: '/user/signUp',
+    name: "signUp",
+    component: SignUp
+},
+{
+    path: '/user/home',
+    name: "home",
+    component: Home
+}
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
